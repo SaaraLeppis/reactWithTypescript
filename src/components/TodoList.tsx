b7fa107b14f183adf20278d265241f9ab210e2a9
Saara Leppis <saara@leppihalme.fi>
2022-03-04 12:28:33 +0200

    delete functionality added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,6 +5,7 @@ interface ToDoListProps {
         id: string; 
         text:string
     }[]; 
+    onDeleteTodo: (id:string) =>void;  
 }
 
 const TodoList: React.FC<ToDoListProps> =(props)=>{
@@ -12,7 +13,8 @@ const TodoList: React.FC<ToDoListProps> =(props)=>{
     return (
         <ul>
             {props.items.map((todo)=>(
-                <li key={todo.id}>{todo.text}</li>
+                <li key={todo.id}><span>{todo.text}</span>
+                <button onClick={props.onDeleteTodo.bind(null, todo.id)}>Delete</button></li>
             ))}
         </ul>
     )
